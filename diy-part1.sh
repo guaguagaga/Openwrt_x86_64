8f456249b3bc390089d8a02b6a339636ab9c85f5
guaguagaga <42617543+guaguagaga@users.noreply.github.com>
2021-07-11 17:14:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,9 @@
 #
 
 # Uncomment a feed source
-sed -i '4asrc-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
+#sed -i '4asrc-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
